cbdde2428f71918f2d781ee972362e6c51a3d05f
linda8850 <lijia9988@qq.com>
2022-06-08 13:23:22 +0800

    part2->exercises2.9->(phonebook)

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [personsToShow, setPersonsToShow] = useState(persons);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const currentName = persons.filter((person) => person.name === newName);
+
+    if (currentName.length === 0) {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      };
+      setPersons(persons.concat(personObject));
+      setPersonsToShow(persons.concat(personObject));
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const filterByName = (event) => {
+    const search = event.target.value;
+    setFilter(search);
+    setPersonsToShow(
+      persons.filter((person) => person.name.toLowerCase().includes(search))
+    );
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <p>
+        filter shown with <input value={filter} onChange={filterByName} />
+      </p>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {personsToShow.map((person) => (
+          <Person key={person.id} person={person} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
